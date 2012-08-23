47a83413214ec20e4f4e1f73e6a2eb49e31aca36
Clopy <clopy@unweb.me>
2012-08-23 01:53:20 -0700

    renamed text commentary to commentary

--- src/criticalcommons/content/content/commentary.py
+++ src/criticalcommons/content/content/commentary.py
@@ -4,7 +4,11 @@ from zope import schema
 from plone.directives import form, dexterity
 
 from plone.app.textfield import RichText
-from plone.namedfile.field import NamedImage
+from z3c.relationfield.schema import RelationList
+from z3c.relationfield.schema import RelationChoice
+from plone.formwidget.autocomplete import AutocompleteFieldWidget
+from plone.formwidget.contenttree import ObjPathSourceBinder
+
 
 from criticalcommons.content import _
 
@@ -17,10 +21,20 @@ class ICommentary(form.Schema):
         )
 
     summary = schema.Text(
-            title=_(u"A short summary"),
+            title=_(u"Commentary summary"),
         )
     
-    textCommentary = RichText(
-            title=_(u"Text commentary"),
+    commentary = RichText(
+            title=_(u"Commentary"),
             required=True
+        )
+
+    form.widget(relatedItems=AutocompleteFieldWidget)
+    relatedItems = RelationList(
+            title=u"Clips",
+            description=_(u"Start typing in the field below to search for clips to which this commentary should be linked."),
+            default=[],
+            value_type=RelationChoice(title=_(u"Related"),
+                                      source=ObjPathSourceBinder()),
+            required=False,
         )
