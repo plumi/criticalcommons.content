66f49cdc7f15fe0c09eee8dc6c04f6af382a25a0
Dimitris Moraitis <dimo@unweb.me>
2012-08-29 04:30:36 -0700

    add subscribers and clip view

---
+++ src/criticalcommons/content/clip.py
@@ -0,0 +1,31 @@
+from zope.component import adapts
+from zope.interface import implements
+from archetypes.schemaextender.interfaces import ISchemaExtender
+from plumi.content.interfaces import IPlumiVideo
+from Products.Archetypes.public import StringField, ReferenceField
+from Products.Archetypes.public import StringWidget, ReferenceWidget
+from archetypes.schemaextender.field import ExtensionField
+
+class MyStringField(ExtensionField, StringField):
+    """" just a string field """
+
+class MyReferenceField(ExtensionField, ReferenceField):
+    """ just a reference field """
+
+class ClipExtender(object):
+    """ schema extender for Plumi Videos, adds filmName & commentaries """
+    adapts(IPlumiVideo)
+    implements(ISchemaExtender)
+
+    fields = [
+             MyStringField("filmName",
+		widget = StringWidget(label="Name of film", description="Name of the film that the clip comes from")),
+             ]
+
+    def __init__(self, context):
+        self.context = context
+
+    def getFields(self):
+        return self.fields
+
+
