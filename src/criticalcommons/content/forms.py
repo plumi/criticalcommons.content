7b56910cb1f3e72229d37db2874da0ebcee485ae
Dimitris Moraitis <dimo@unweb.me>
2012-08-30 09:13:27 -0700

    add commentary form handler

--- src/criticalcommons/content/forms.py
+++ src/criticalcommons/content/forms.py
@@ -8,6 +8,10 @@ from plone.app.z3cform.wysiwyg import WysiwygFieldWidget
 from criticalcommons.content import _
 from plumi.content.interfaces import IPlumiVideo
 from plone.z3cform.layout import wrap_form
+from plone.i18n.normalizer.interfaces import IIDNormalizer
+from Products.CMFCore.utils import getToolByName
+from zope.component import getUtility
+from plone.app.textfield.value import RichTextValue
 
 
 class ICommentary(Interface):
@@ -21,7 +25,32 @@ class CommentaryForm(form.Form):
 
     @button.buttonAndHandler(_(u'Save changes'), name='apply')
     def handleApply(self, action):
-        print "inside save"
-        import pdb;pdb.set_trace()
+        pm = getToolByName(self.context, 'portal_membership')
+        home = pm.getHomeFolder()
+        if not home: 
+            return
+        title = self.request['form.widgets.title']
+        body = self.request['form.widgets.body']
+        existing = [i for i in self.context.getRelatedItems() if i.portal_type=='Commentary']
+        dupes = [d for d in existing if d.Title() == title]
+        if len(dupes):
+            self.status = _(u"A commentary with the same title already exists")
+            return
+        normalizer = getUtility(IIDNormalizer)
+        try:
+            target_folder = home.commentaries
+            new_id = target_folder.invokeFactory(id=str(DateTime().millis()), type_name='Commentary', title=title)
+            obj = target_folder[new_id]
+            obj.textCommentary = RichTextValue(unicode(body), 'text/html', 'text/html', 'utf-8')
+            self.context.setRelatedItems(self.context.getRelatedItems() + [obj])
+        except Exception as e:
+            self.status = _(u"Failed to publish commentary")
+            print e
+            return
+
+        wft = getToolByName(self.context, 'portal_workflow')
+        state = wft.getInfoFor(self.context, 'review_state')
+        if 'state' == private:
+            wft.doActionFor(self.context, 'submit')
 
 CommentaryView = wrap_form(CommentaryForm)
