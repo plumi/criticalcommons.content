8ec689e4026e8dfd9935e80c14f19ea685e24bfd
Dimitris Moraitis <dimo@unweb.me>
2012-09-12 07:15:35 -0700

    fixes

--- src/criticalcommons/content/subscribers.py
+++ src/criticalcommons/content/subscribers.py
@@ -4,29 +4,31 @@ from Products.CMFCore.utils import getToolByName
 from criticalcommons.content.content.commentary import ICommentary
 
 def removeCommentary(obj, event):
-    for item in obj.relatedItems:
-        video = item.to_object
-        rel = video.getRelatedItems()
-        # remove obj from related video's relatedItems field
-        for r in rel:
-            if r == obj:
-                rel.remove(r)
-                video.setRelatedItems(rel)
+    try:
+        for item in obj.relatedItems:
+            video = item.to_object
+            rel = video.getRelatedItems()
+            # remove obj from related video's relatedItems field
+            for r in rel:
+                if r == obj:
+                    rel.remove(r)
+                    video.setRelatedItems(rel)
 
-        # if no other commentaries in video make video private
-        relCom = [r for r in rel if r.portal_type=='Commentary']
-        if not len(relCom):
-            wft = getToolByName(video, 'portal_workflow')
-            review_state = wft.getInfoFor(video, 'review_state')
-            if review_state == 'featured':
-                wft.doActionFor(video, 'unfeature')
+            # if no other commentaries in video make video private
+            relCom = [r for r in rel if r.portal_type=='Commentary']
+            if not len(relCom):
+                wft = getToolByName(video, 'portal_workflow')
                 review_state = wft.getInfoFor(video, 'review_state')
-            if review_state == 'published':
-                wft.doActionFor(video, 'retract')
-                wft.doActionFor(video, 'hide')
-            elif review_state == 'pending':
-                wft.doActionFor(video, 'hide')
-
+                if review_state == 'featured':
+                    wft.doActionFor(video, 'unfeature')
+                    review_state = wft.getInfoFor(video, 'review_state')
+                if review_state == 'published':
+                    wft.doActionFor(video, 'retract')
+                    wft.doActionFor(video, 'hide')
+                elif review_state == 'pending':
+                    wft.doActionFor(video, 'hide')
+    except:
+        pass      
 
 def modifyCommentary(obj, event):
     for item in obj.relatedItems:
