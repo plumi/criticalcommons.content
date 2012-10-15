b206ceeb37ea56c3fc3464b38c296066716b0669
Clopy <clopy@unweb.me>
2012-10-15 03:51:05 -0700

    Fixed the autocomplete widget

--- src/criticalcommons/content/interfaces.py
+++ src/criticalcommons/content/interfaces.py
@@ -2,3 +2,7 @@ from zope.interface import Interface
 
 class ILectureLibrary(Interface):
     """" Marker interface for lecture library collection """
+
+class ICCAutocompleteWidget(Interface):
+    """Marker interface for the autocomplete widget
+    """
