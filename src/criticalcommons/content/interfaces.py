40730dfbb7b839fa4ef534e161b3bb216ead8178
Dimitris Moraitis <dimo@unweb.me>
2012-10-15 03:52:09 -0700

    remove a stale entry

--- src/criticalcommons/content/interfaces.py
+++ src/criticalcommons/content/interfaces.py
@@ -2,7 +2,3 @@ from zope.interface import Interface
 
 class ILectureLibrary(Interface):
     """" Marker interface for lecture library collection """
-
-class ICCAutocompleteWidget(Interface):
-    """Marker interface for the autocomplete widget
-    """
