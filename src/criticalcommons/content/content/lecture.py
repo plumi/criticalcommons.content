55524bf58959b6bcabf2235b950b49ed90713f11
Markos Gogoulos <mgogoulos@unweb.me>
2013-05-01 03:08:16 -0700

    lectures widget

--- src/criticalcommons/content/content/lecture.py
+++ src/criticalcommons/content/content/lecture.py
@@ -12,6 +12,7 @@ from plone.formwidget.contenttree import ObjPathSourceBinder
 from plone.namedfile.field import NamedImage
 from plumi.content.interfaces.plumivideo import IPlumiVideo
 from z3c.form.browser.checkbox import CheckBoxFieldWidget
+from z3c.formwidget.query.widget import QuerySourceFieldCheckboxWidget
 
 from criticalcommons.content import _
 
@@ -30,12 +31,12 @@ class ILecture(form.Schema):
             description=_(u"The thumbnail image for the lecture"),
         )
 
-    form.widget(relatedItems=AutocompleteMultiFieldWidget)
-    relatedItems = RelationList(
+    form.widget(relatedItems=QuerySourceFieldCheckboxWidget)
+    relatedItems = schema.List(
             title=u"Clips",
             description=_(u"Select clips to create a playlist that accompanies this lecture. Start typing to search for relevant clips. You may include as many clips as you want. Clips may be added or deleted at any time."),
             default=[],
-            value_type=RelationChoice(title=_(u"Related"),
+            value_type=schema.Choice(title=_(u"Related"),
                        source=ObjPathSourceBinder(object_provides=IPlumiVideo.__identifier__)),
             required=False,
         )
